c502c8ad5ebbc7163ea22259299b5901f1ab33b9
Ychi <ychi7799@gmail.com>
2022-07-05 11:19:15 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// window.alert(5 + 6);
+// $(document).ready(function(){
+// //   $("#row").html("<td>123</td>");
+//     $.ajax({
+//         url: 'http://10.204.16.106:9316/TB/device/list.json',
+//         type: "get",
+// 		dataType:"json",
+//         success: function (info) {
+// 			console.log(info);				
+//         },
+//         error: function (data) {
+//             console.log("請求失敗");
+//         }
+//     });
+// });
