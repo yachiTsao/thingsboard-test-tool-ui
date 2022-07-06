07133bac20117327c9dab48f507f54cdc9ad2273
Ychi <ychi7799@gmail.com>
2022-07-06 13:25:59 +0800

    feat: add ceate-device-form function

--- src/link-api.js
+++ src/link-api.js
@@ -1,9 +1,46 @@
-datas=$("form").serizlizeArray()
-$.ajax({
-	url:"http://10.204.16.106:9316/TB/device/create",
-	method:"post",
-	data:datas,  // data指定datas,剛剛抓出的資料（上面的陣列）
-	success:function(res){
-		console.log(res) // 接回成功後會有回傳值
+$("#create-device-form").submit(function (event) {
+    event.preventDefault();
+
+    const datas = $(this).serializeArray();
+    console.log(datas);
+
+    // console.log(parseData(datas));
+    $.ajax({
+        url: "http://10.204.16.106:9316/TB/device/create",
+        method: "post",
+        dataType: "json",
+        contentType: "application/json",
+        data: parseData(datas),
+        success: function (res) {
+            console.log(res);
+        }
+    });
+
+});
+
+function parseData(data) {
+    // const newObj = {
+    //     deviceCount: +(data[0].value)
+    // };
+    return JSON.stringify({
+        "deviceCount": +(data[0].value),
+        "deviceName": data[1].value,
+        "deviceType": data[2].value
+    });
 }
-})
+
+var datas = $(this).serializeArray();
+
+// const newObj = {
+//     deviceCount: +(datas[0].value),
+//     deviceName: "TB-tools2",
+//     deviceType: "ggg"
+// };
+
+
+// $("#send-create-device").on('click', function (e) {
+//     e.preventDefault();
+//     console.log($("#create-device-form"));
+//     datas = $("#create-device-form").serizlizeArray();
+//     console.log(datas);
+// })
