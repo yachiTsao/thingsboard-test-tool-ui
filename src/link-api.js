f1a4a64993357f87dc8fe451312af8c3acc40e71
Ychi <ychi7799@gmail.com>
2022-07-06 14:11:06 +0800

    feat: change link-api.js mark

--- src/link-api.js
+++ src/link-api.js
@@ -1,46 +1,32 @@
 $("#create-device-form").submit(function (event) {
     event.preventDefault();
-
+    //將資料整理成陣列
     const datas = $(this).serializeArray();
     console.log(datas);
 
-    // console.log(parseData(datas));
+    //
     $.ajax({
         url: "http://10.204.16.106:9316/TB/device/create",
         method: "post",
+        //上傳json格式需加入以下兩行
         dataType: "json",
         contentType: "application/json",
         data: parseData(datas),
         success: function (res) {
-            console.log(res);
+            alert("已成功新增資料");
+        },
+        error: function (data) {
+            alert("新增資料失敗");
         }
     });
 
 });
-
+//將資料轉為json格式
 function parseData(data) {
-    // const newObj = {
-    //     deviceCount: +(data[0].value)
-    // };
     return JSON.stringify({
+        //將deviceCount轉成數字格式
         "deviceCount": +(data[0].value),
         "deviceName": data[1].value,
         "deviceType": data[2].value
     });
 }
-
-var datas = $(this).serializeArray();
-
-// const newObj = {
-//     deviceCount: +(datas[0].value),
-//     deviceName: "TB-tools2",
-//     deviceType: "ggg"
-// };
-
-
-// $("#send-create-device").on('click', function (e) {
-//     e.preventDefault();
-//     console.log($("#create-device-form"));
-//     datas = $("#create-device-form").serizlizeArray();
-//     console.log(datas);
-// })
