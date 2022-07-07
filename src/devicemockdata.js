4aa106549b2cec89c1cde22154f00209e89b12cc
Ychi <ychi7799@gmail.com>
2022-07-07 14:06:23 +0800

    feat: modify function and object name

--- src/devicemockdata.js
+++ src/devicemockdata.js
@@ -1,50 +1,57 @@
-function mockdatasubmit(event) {
+
+function mockDataSubmit(event) {
     //清除預設
     event.preventDefault();
     //抓form裡面的資料
     const data = new FormData(event.target);
-    console.log(parseData(data));
+    //若觸發一次parseData()就會跑一次alert 以此類堆
+    const newData = parseData(data);
 
-    $.ajax({
-        url: "http://10.204.16.106:9316/TB/device/data/setting/create",
-        method: "post",
-        //上傳json格式需加入以下兩行
-        dataType: "json",
-        contentType: "application/json",
-        data: parseData(data),
-        success: function (res) {
-            alert("已成功新增資料");
-        },
-        error: function (data) {
-            alert("新增資料失敗");
-        }
-    });
-}
-//將資料轉為json格式
-function parseData(dataset) {
-    const mockdataname = dataset.get('name');
-    const mockdata = dataset.get('data');
-    const isJsondata = isJson(mockdata);
-    console.log(isJsondata);
-    const Jsondata = jQuery.parseJSON(mockdata);
-    console.log(isJsondata);
-    return JSON.stringify({
-        "name": mockdataname,
-        "data": Jsondata,
-    });
-}
-//測試輸入文字是否為json格式
-function isJson(str) {
-    if (typeof str == 'string') {
-        try {
-            if (typeof JSON.parse(str) == "object") {
-                return str;
+    if (newData) {
+        $.ajax({
+            url: "http://10.204.16.106:9316/TB/device/data/setting/create",
+            method: "post",
+            //上傳json格式需加入以下兩行
+            dataType: "json",
+            contentType: "application/json",
+            data: newData,
+            success: function (res) {
+                alert("已成功新增資料");
+            },
+            error: function (data) {
+                alert("新增資料失敗");
             }
-        } catch (e) {
-            alert("請輸入Json格式");
-            throw Error("This isn't json.");
-        }
+        });
     }
 }
-const mockdataform = document.querySelector('#device-mockdata-form');
-mockdataform.addEventListener('submit', mockdatasubmit);
+//將資料以json格式組合
+function parseData(dataset) {
+    const mockDataName = dataset.get('name');
+    const mockData = dataset.get('data');
+    const JsonData = parseJson(mockData);
+    if (JsonData) {
+        return JSON.stringify({
+            "name": mockDataName,
+            "data": JsonData,
+        });
+    }
+    return;
+}
+//測試輸入文字是否為json格式
+function parseJson(str) {
+    //先把非法的可能性排除
+    if (typeof str !== 'string') return;
+    try {
+        //將字串轉為Json格式
+        const parse = JSON.parse(str);
+        //array object 皆為 object 
+        if (typeof parse !== "object") return;
+        return parse;
+    } catch (e) {
+        alert("請輸入合法Json格式");
+        //回傳空值
+        return;
+    }
+}
+const mockDataForm = document.querySelector('#device-mockData-form');
+mockDataForm.addEventListener('submit', mockDataSubmit);
