836b5cbc1ba3b9dee2b2cecce44e2de718151ae7
Ychi <ychi7799@gmail.com>
2022-07-13 18:10:16 +0800

    feat: modify device action and function

--- src/deviceAction.js
+++ src/deviceAction.js
@@ -40,39 +40,39 @@
 // const actionDataForm = document.querySelector('#device-actionData-form');
 // actionDataForm.addEventListener('submit', actionDataFormSubmit);
 
-let isSendData = false;
-let isSubscribeRPC = false;
+
+// console.log(isSubscribeRPC[1]);//undefined
 // 搜尋action 
-function findArrayItem(arr) {
-    //
-    if (Array.isArray(arr) && arr.length > 0) {
-        const res1 = arr.find(element => element === ('subscribeRPC'));
-        // const res = arr.find(element => element === ('sendData'));
-        console.log(res1);
-        // if (res === 'sendData') {
-        //     // console.log('true');
-        //     return true;
-        // }
-        if (res1 === ('subscribeRPC')){
-            return true;
-        }
-        return false;
-    }
-    return false;
+function findArrayItem(arr, target) {
+    if (!Array.isArray(arr) || arr.length === 0) return false;
+    const res1 = arr.find(element => element === target);
+    if (!res1) return false;
+    return true;
+    // return arr.find(element => element === target) ? true : false; 
 }
 
 // sendData, subscribeRPC 
 //create table and build items
-function tableItemBuilder(device, index) { 
-    console.log(device);
-    isSubscribeRPC = findArrayItem(device.action);
-    console.log(isSubscribeRPC);
+function tableItemBuilder(device, index) {
+    const isSendData = findArrayItem(device.action, 'sendData');
+    const isSubscribeRPC = findArrayItem(device.action, 'subscribeRPC');
+    // console.log(device.sendDataFrequency);
     return `<tr><td> ${device.name} </td><td> ${device.type} </td>
-    <td><input name=actions id=subscribeRPC-${index} type=checkbox class=form-check-input checked=${isSubscribeRPC}> SubscribeRPC </input><br>
-    <input name=actions id=sendData-${index} type=checkbox class=form-check-input checked=${isSendData}> SendData </input></td>
+    <td><input name=actions id=subscribeRPC-${index} type=checkbox class=form-check-input ${isSubscribeRPC ? 'checked' : ''}> SubscribeRPC </input><br>
+    <input name=actions id=sendData-${index} type=checkbox class=form-check-input ${isSendData ? 'checked' : ''}> SendData </input></td>
     <td><input name=second id=second-${index} class='form-control second' type=number value= ${device.sendDataFrequency} ></input></td></tr>`
 }
 
+function checkboxValue(e,i,target){
+    e.target.value = e.target.checked;
+    console.log(e.target.checked, e.target.value);
+
+    if (e.target.checked === true) { //如果是true就加入(原本沒有)
+        deviceList[i].action.push(target);
+    } else { //不是sendData的留下(原本有)
+        deviceList[i].action = deviceList[i].action.filter(element => element !== target);
+    }
+}
 
 //更新Table
 function updateTable(deviceList) {
@@ -80,44 +80,50 @@ function updateTable(deviceList) {
     let table = '';
     for (let i = 0; i < deviceList.length; i++) {
         table += tableItemBuilder(deviceList[i], i);
+        console.log(deviceList[i].action);
     }
-    console.log(deviceList);
+    // console.log(deviceList.action);
     // $('#device-action-data').html(table);  //jQuery寫法
     tablePage.innerHTML = table; //js寫法
 
-    // const actionArray = [];
+    
 
-    // for (let i = 0; i < deviceList.length; i++) {
+    for (let i = 0; i < deviceList.length; i++) {
+        // const sendDataCallback = checkboxValue(e,i,'');
+        function sendDataCallback (e) {
+            e.target.value = e.target.checked;
+            console.log(e.target.checked, e.target.value);
+            
+            if (e.target.checked === true) {
+                deviceList[i].action.push('sendData');
+            } else {//不是subscribeRPC(原本有)
+                deviceList[i].action = deviceList[i].action.filter(element => element !== 'sendData');
+            }
+            console.log(deviceList);
+        }
+        $(`#sendData-${i}`).on('click', sendDataCallback); //sendDataCallback(e) = checkboxValue(e,i,'')(e)
+        console.log(deviceList);
 
-    //     actionArray[i] = [];
-    //     $(`#sendData-${i}`).on('click', (e) => {
-    //         //將點選結果覆蓋並顯示
-    //         // beforeAction = e.target.value
-    //         // e.target.value = e.target.checked;
-    //         // afterAction = e.target.value;
-    //         // console.log(beforeAction, afterAction);
-    //         // if (afterAction !== beforeAction){
-    //         //     console.log("change");
-    //         //     document.getElementById(`sendData-${i}`).checked = afterAction;
-    //         // }
-    //         e.target.value = e.target.checked;
-    //         console.log(e.target.checked, e.target.value);
-    //         actionArray[i][0] = e.target.value;
-    //         console.log(actionArray);
-    //     });
-    //     $(`#subscribeRPC-${i}`).on('click', (e) => {
-    //         e.target.value = e.target.checked;
-    //         console.log(e.target.checked, e.target.value);
-    //         actionArray[i][1] = e.target.value;
-    //         console.log(actionArray);
-    //     });
-    //     $(`#second-${i}`).on('change', (e) => {
-    //         console.log(e.target.value);
-    //         // document.getElementById(`second-${i}`).value;
-    //         actionArray[i][2] = e.target.value;
-    //         console.log(actionArray);
-    //     });
-    // }
+        function subscribeRPCCallback (e) {
+            e.target.value = e.target.checked;
+            console.log(e.target.checked, e.target.value);
+            
+            if (e.target.checked === true) {
+                deviceList[i].action.push('subscribeRPC');
+            } else {//不是subscribeRPC(原本有)
+                deviceList[i].action = deviceList[i].action.filter(element => element !== 'subscribeRPC');
+            }
+            console.log(deviceList);
+        }
+        $(`#subscribeRPC-${i}`).on('click', subscribeRPCCallback); // subscribeRPCCallback(event)
+
+        $(`#second-${i}`).on('change', (e) => {
+            // console.log(e.target.value);
+            deviceList[i].sendDataFrequency = parseInt(e.target.value);
+            console.log(deviceList);
+        });
+
+    }
 }
 //連接API
 async function fetchDeviceActionListAndUpdateTable() {
